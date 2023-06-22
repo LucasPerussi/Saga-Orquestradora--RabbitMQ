a66b56d0102e10627995825b57d85bd65919921c
Lucas Perussi <perussilucas@hotmail.com>
2023-06-22 20:04:30 -0300

    tratamento para exibir objetos contidos em um json

--- sagas/listeners/src/main/java/com/listeners/consumers/ContaConsumer.java
+++ sagas/listeners/src/main/java/com/listeners/consumers/ContaConsumer.java
@@ -8,6 +8,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.listeners.constantes.RabbitMQConstantes;
 import com.listeners.dto.ContaDTO;
+import com.listeners.dto.MovimentacaoDTO;
 
 
 @Component
@@ -17,6 +18,7 @@ public class ContaConsumer {
 	@RabbitListener(queues = RabbitMQConstantes.FILA_CONTA)
 	private void consumidor(String mensagem) throws JsonProcessingException, InterruptedException, IllegalArgumentException {
 		ContaDTO contaDTO = new ObjectMapper().readValue(mensagem, ContaDTO.class);
+//		MovimentacaoDTO movimentacoes = new ObjectMapper().readValue(mensagem, MovimentacaoDTO.class);
 		System.out.println("------------CONTA--------------------");
 		System.out.println(contaDTO.id);
 		System.out.println(contaDTO.clienteId);
@@ -25,7 +27,28 @@ public class ContaConsumer {
 		System.out.println(contaDTO.saldo);
 		System.out.println(contaDTO.limite);
 		System.out.println(contaDTO.dataCriacao);
-		System.out.println(contaDTO.movimentacoes);
+//		System.out.println(contaDTO.movimentacoes);
+		if (contaDTO.movimentacoes != null) {
+		    	for (MovimentacaoDTO movimentacao : contaDTO.movimentacoes) {
+				    System.out.println("------------MOVIMENTAÇÃO-----------------");
+				    System.out.println(movimentacao.id);
+				    System.out.println(movimentacao.data);
+				    System.out.println(movimentacao.tipo);
+				    System.out.println(movimentacao.idOrigem);
+				    System.out.println(movimentacao.idDestino);
+				    System.out.println("----------------------------------------");
+				}
+		}else {
+				MovimentacaoDTO movimentacao = (MovimentacaoDTO) contaDTO.movimentacoes;
+			 	System.out.println("------------MOVIMENTAÇÃO-----------------");
+			    System.out.println(movimentacao.id);
+			    System.out.println(movimentacao.data);
+			    System.out.println(movimentacao.tipo);
+			    System.out.println(movimentacao.idOrigem);
+			    System.out.println(movimentacao.idDestino);
+			    System.out.println("----------------------------------------");
+		}
+		
 		System.out.println("---------------------------------");
 		
 //		throw new IllegalArgumentException("Argumento Inválido");
