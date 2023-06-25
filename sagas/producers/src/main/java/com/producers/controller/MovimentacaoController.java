a1948c5d73364a9fe864114dd9ef227b90a4035f
Lucas Perussi <perussilucas@hotmail.com>
2023-06-25 10:57:44 -0300

    post nos request do producer

--- sagas/producers/src/main/java/com/producers/controller/MovimentacaoController.java
+++ sagas/producers/src/main/java/com/producers/controller/MovimentacaoController.java
@@ -2,6 +2,7 @@ package com.producers.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,7 +22,7 @@ public class MovimentacaoController {
 	RabbitMQService rabbitMQService;
 	
 //	envia mensagem para a fila
-	@PutMapping
+	@PostMapping
 	private ResponseEntity<?> novoGerente(@RequestBody MovimentacaoDTO movimentacaoDTO) {
 //		System.out.println(gerenteDTO.codigoProduto);
 		this.rabbitMQService.enviaMensagem(RabbitMQConstantes.FILA_MOVIMENTACAO, movimentacaoDTO);
